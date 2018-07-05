b288e2bbd816af9444d5b266383156f8fb31f7e2
Georgi Stalev <georgi_stalev@abv.bg>
2018-07-05 22:59:03 +0300

    products

--- Shop-Stop/handlers/home.js
+++ Shop-Stop/handlers/home.js
@@ -1,17 +1,21 @@
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
+const database = require('../config/database')
+const qs = require('querystring')
 
 module.exports = (req, res) => {
-    req.pathName = req.pathName || url.parse(req.url).pathName
+    req.pathName = req.pathName || url.parse(req.url)
 
-    if(req.pathName === '/' && req.method === 'GET') {
+
+    if (req.url === '/' && req.method === 'GET') {
+
         //"path" module is just utility module for editing file paths
         let filePath = path.normalize(
             path.join(__dirname, '../views/home/index.html'))
 
-            fs.readFile(filePath, (err, data) => {
-            if(err) {
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
                 console.log(err)
                 res.writeHead(404, {
                     'Content-Type': 'text/plain'
@@ -25,8 +29,28 @@ module.exports = (req, res) => {
             res.writeHead(200, {
                 'Content-Type': 'text/html'
             })
-            
-            res.write(data)
+
+            //let queryData = qs.parse(url.parse(req.url).query)
+            //console.log(queryData)
+            let products = database.products.getAll()
+            //   if (queryData.query) {
+            //       products = products.filter(p => p.name.toLowerCase().includes(queryData))
+            //       console.log(queryData);
+            //   }
+            let content = ''
+            for (let product of products) {
+                content +=
+                    `<div class="product-card">
+                        <img class="product-img" src=${product.image} alt="picture">
+                        <h2>${product.name}</h2>
+                        <p>${product.description}</p>
+                        <p>${product.price}$</p>
+                        </div>`
+            }
+
+            let html = data.toString().replace('{content}', content)
+
+            res.write(html)
             res.end()
         })
     } else {
--- Shop-Stop/handlers/index.js
+++ Shop-Stop/handlers/index.js
@@ -1,3 +1,5 @@
 const homeHandler = require('./home')
+const staticHandler = require('./static')
+const productHandler = require('./product')
 
-module.exports = [ homeHandler ]
+module.exports = [homeHandler, staticHandler, productHandler]
